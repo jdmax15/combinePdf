d5215223dd69809110a2b3f327df3ae8857beed9
jdmax15 <joel.maxwell15@gmail.com>
2024-11-09 08:11:36 +1100

    Added code to iterate over each located .pdf file.

--- combinePdfs.py
+++ combinePdfs.py
@@ -13,8 +13,10 @@ pdfFiles.sort(key = str.lower)
 
 pdfWriter = PyPDF2.PdfFileWriter()
 
-# TODO: Loop through all the PDF files.
-
-# TODO: Loop through all the pages (except the first) and add them.
+# Loop through all the PDF files.
+for filename in pdfFiles:
+    pdfFileObj = open(filename, 'rb')
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+    # Loop through all the pages (except the first) and add them.
 
 # TODO: Save the resulting PDF to a file.
